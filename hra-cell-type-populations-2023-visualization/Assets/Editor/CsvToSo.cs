af8e4b3c5d03a478c1fcaadf17ae997eeb0848ee
Andreas Bueckle <abueckle@iu.edu>
2023-02-16 10:28:48 -0500

    Added code for CSV ingest

---
+++ hra-cell-type-populations-2023-visualization/Assets/Editor/CsvToSo.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class CsvToSo : MonoBehaviour
+{
+    private static string root = "Assets/Resources/Cells";
+
+    [MenuItem("Utilities/IngestCellPositions")]
+
+    public static void GetCorrelationMatrices()
+    {
+
+        foreach (string file in Directory.GetFiles(root, "*.csv"))
+        {
+            string[] allLines = File.ReadAllLines(file);
+            int counter = 0;
+
+            foreach (var line in allLines)
+            {
+                if (line.Split(',')[0] == "organ") continue;
+                if (line.Split(',')[1] != "VH_F_kidney_capsule_R") continue;
+                if (counter > 20000) return;
+
+                SO_CellTypeCount count = ScriptableObject.CreateInstance<SO_CellTypeCount>();
+                count.organ = line.Split(',')[0];
+                count.anatomicalStructure = line.Split(",")[1];
+                count.cellType = line.Split(",")[2];
+                count.position = new Vector3(
+                    float.Parse(line.Split(',')[3]),
+                    float.Parse(line.Split(',')[4]),
+                    float.Parse(line.Split(',')[4])
+                    );
+
+                AssetDatabase.CreateAsset(count, $"Assets/ScriptableObjects/{count.anatomicalStructure}_{counter}.asset");
+                counter++;
+            }
+        }
+        AssetDatabase.SaveAssets();
+    }
+}
---
+++ hra-cell-type-populations-2023-visualization/Assets/ScriptableObjects/SO_CellTypeCount.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SO_CellTypeCount : ScriptableObject
+{
+    public string organ, anatomicalStructure, cellType;
+    public Vector3 position;
+
+    //public SO_CellTypeCount(string organ, string anatomicalStructure, string type, Vector3 pos) => (this.organ, this.anatomicalStructure, cellType, this.position) = (organ, anatomicalStructure, type, pos);
+
+}
+
+
