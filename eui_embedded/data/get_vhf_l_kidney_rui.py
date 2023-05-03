e01a3d9b250981a0a2bbafa71b70fbd806e872de
Andreas Bueckle <abueckle@iu.edu>
2023-05-03 16:35:56 -0400

    Added web component instance of EUI

--- eui_embedded/data/get_vhf_l_kidney_rui.py
+++ eui_embedded/data/get_vhf_l_kidney_rui.py
@@ -6,28 +6,46 @@ def main():
 
     # first, let's get the most recent data
     # To get TOKEN for viewing unpublished data, go to EUI, log in, then view source, then copy token from browser
-    TOKEN = ""
+  
     endpoint = "https://ccf-api.hubmapconsortium.org/v1/hubmap/rui_locations.jsonld"
-    headers = {"Authorization": "Bearer " + TOKEN}
-    data = requests.get(endpoint, headers=headers).json()
+    
+    # data = requests.get(endpoint, headers=headers).json()
     # print(data)
     # data = json.load("rui_locations.jsonld")
-    
-    search_string = 'VHFLeftKidney'
-    # template = 
-    
-    counter = 0
-    for donor in data['@graph']:
-        for sample in donor['samples']:
-            if search_string not in sample['rui_location']['placement']['target']:
-                counter = counter +1
-                sample['rui_location'] = ""
-    print(counter)
-    print(data)
-    # with open("rui_locations.jsonld", "w") as outfile:
 
-    #     json.dump(data, outfile)
-           
+    endpoints = [
+        "https://ccf-api.hubmapconsortium.org/v1/hubmap/rui_locations.jsonld",
+        "https://ccf-api.hubmapconsortium.org/v1/gtex/rui_locations.jsonld"
+    ]
+
+ 
+    
+    get_data(endpoints[0], 'hubmap')
+    get_data(endpoints[1], 'gtex')
+       
+  
+def get_data(url, consortium):
+    TOKEN = ""
+    search_string = 'VHFLeftKidney'
+    headers = {"Authorization": "Bearer " + TOKEN}
+    data = requests.get(url, headers=headers).json()
+    newData = []
+    for donor in data['@graph']:
+        newSamples = []
+        for sample in donor['samples']:
+            if search_string in sample['rui_location']['placement']['target']:
+             newSamples.append(sample)
+
+        if len(newSamples) > 0:
+            newData.append(donor);
+            donor['samples'] = newSamples
+            
+    data['@graph'] = newData
+
+    # print(data)
+    with open("rui_locations_" + consortium + ".jsonld", "w") as outfile:
+        json.dump(data, outfile, ensure_ascii=False, indent=4)
+
 # driver code
 if __name__ == '__main__':
     main()
